5645b115b8618bf33cd2f4907cd48c599e76fae4
Nicolas Brousse <nicolas@brousse.info>
2010-08-13 17:07:20 -0700

    remove 1000000 entries test from judy-bench (too much memory used for normal array)

--- judy-bench.php
+++ judy-bench.php
@@ -9,7 +9,7 @@
     }
 
     $v = $argv[1];
-    $count = array(100, 1000, 10000, 100000, 1000000);
+    $count = array(100, 500, 1000, 5000, 10000, 50000, 100000, 500000);
 
 foreach($count as $v) {
     echo "## Count: $v\n";
