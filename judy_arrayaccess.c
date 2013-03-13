2e7eadaa7f7bf587b8d15f25a8fa11063f1c11ff
Antony Dovgal <tony@daylessday.org>
2013-03-13 18:01:23 +0400

    spaces -> tabs, that's the standard

--- judy_arrayaccess.c
+++ judy_arrayaccess.c
@@ -20,311 +20,311 @@
 #include "judy_arrayaccess.h"
 
 /* {{{ proto int Judy::offsetSet(mixed offset, mixed value)
- set the value at the given offset in the Judy Array */
+   set the value at the given offset in the Judy Array */
 PHP_METHOD(judy, offsetSet)
 {
-    JUDY_METHOD_GET_OBJECT
+	JUDY_METHOD_GET_OBJECT
 
-    if (intern->type == TYPE_BITSET)
-    {
-        zval        *zindex;
-        Word_t		index;
-        zend_bool	value;
-        int         Rc_int;
+		if (intern->type == TYPE_BITSET)
+		{
+			zval        *zindex;
+			Word_t		index;
+			zend_bool	value;
+			int         Rc_int;
 
-        if (zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC, "z!b", &zindex, &value) == FAILURE) {
-            RETURN_FALSE;
-        }
+			if (zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC, "z!b", &zindex, &value) == FAILURE) {
+				RETURN_FALSE;
+			}
 
-        if (!zindex || Z_LVAL_P(zindex) <= -1){
-        	index = -1;
-    		J1L(Rc_int, intern->array, index);
-    		if (Rc_int == 1) {
-    			index += 1;
+			if (!zindex || Z_LVAL_P(zindex) <= -1){
+				index = -1;
+				J1L(Rc_int, intern->array, index);
+				if (Rc_int == 1) {
+					index += 1;
+				} else {
+					RETURN_FALSE;
+				}
+			} else {
+				index = Z_LVAL_P(zindex);
+			}
+
+			if (value == 1) {
+				J1S(Rc_int, intern->array, index);
+			} else {
+				J1U(Rc_int, intern->array, index);
+			}
+			RETURN_BOOL(Rc_int);
+		} else if (intern->type == TYPE_INT_TO_INT) {
+			zval        *zindex;
+			Word_t      index;
+			Word_t      value;
+			Word_t      *PValue;
+
+			if (zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC, "z!l", &zindex, &value) == FAILURE) {
+				RETURN_FALSE;
+			}
+
+			if (!zindex || Z_LVAL_P(zindex) <= -1){
+				index = -1;
+				JLL(PValue, intern->array, index);
+				if (PValue != NULL && PValue != PJERR) {
+					index += 1;
+				} else {
+					RETURN_FALSE;
+				}
+			} else {
+				index = Z_LVAL_P(zindex);
+			}
+
+			JLI(PValue, intern->array, index);
+			if (PValue != NULL && PValue != PJERR) {
+				*PValue = value;
+				RETURN_TRUE;
 			} else {
 				RETURN_FALSE;
 			}
-        } else {
-        	index = Z_LVAL_P(zindex);
-        }
+		} else if (intern->type == TYPE_INT_TO_MIXED) {
+			zval        *zindex;
+			Word_t      index;
+			zval        *value;
+			Pvoid_t     *PValue;
 
-        if (value == 1) {
-        	J1S(Rc_int, intern->array, index);
-        } else {
-        	J1U(Rc_int, intern->array, index);
-        }
-        RETURN_BOOL(Rc_int);
-    } else if (intern->type == TYPE_INT_TO_INT) {
-    	zval        *zindex;
-        Word_t      index;
-        Word_t      value;
-        Word_t      *PValue;
+			if (zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC, "z!z", &zindex, &value) == FAILURE) {
+				RETURN_FALSE;
+			}
 
-        if (zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC, "z!l", &zindex, &value) == FAILURE) {
-            RETURN_FALSE;
-        }
+			if (!zindex || Z_LVAL_P(zindex) <= -1){
+				index = -1;
+				JLL(PValue, intern->array, index);
+				if (PValue != NULL && PValue != PJERR) {
+					index += 1;
+				} else {
+					RETURN_FALSE;
+				}
+			} else {
+				index = Z_LVAL_P(zindex);
+			}
 
-        if (!zindex || Z_LVAL_P(zindex) <= -1){
-        	index = -1;
-    		JLL(PValue, intern->array, index);
-            if (PValue != NULL && PValue != PJERR) {
-    			index += 1;
+			JLI(PValue, intern->array, index);
+			if (PValue != NULL && PValue != PJERR) {
+				if (*PValue != NULL) {
+					zval *old_value = (zval *)*PValue;
+					zval_ptr_dtor(&old_value);
+				}
+				*PValue = value;
+				Z_ADDREF_P(*(zval **)PValue);
+				RETURN_TRUE;
 			} else {
 				RETURN_FALSE;
 			}
-        } else {
-        	index = Z_LVAL_P(zindex);
-        }
+		} else if (intern->type == TYPE_STRING_TO_INT) {
+			uint8_t     *key;
+			int         key_length;
+			Word_t      *value;
+			PWord_t     *PValue;
 
-        JLI(PValue, intern->array, index);
-        if (PValue != NULL && PValue != PJERR) {
-            *PValue = value;
-            RETURN_TRUE;
-        } else {
-            RETURN_FALSE;
-        }
-    } else if (intern->type == TYPE_INT_TO_MIXED) {
-    	zval        *zindex;
-        Word_t      index;
-        zval        *value;
-        Pvoid_t     *PValue;
+			if (zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC, "sl", &key, &key_length, &value) == FAILURE) {
+				RETURN_FALSE;
+			}
 
-        if (zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC, "z!z", &zindex, &value) == FAILURE) {
-            RETURN_FALSE;
-        }
-
-        if (!zindex || Z_LVAL_P(zindex) <= -1){
-        	index = -1;
-    		JLL(PValue, intern->array, index);
-            if (PValue != NULL && PValue != PJERR) {
-    			index += 1;
+			JSLI(PValue, intern->array, key);
+			if (PValue != NULL && PValue != PJERR) {
+				*PValue = value;
+				intern->counter++;
+				RETURN_TRUE;
 			} else {
 				RETURN_FALSE;
 			}
-        } else {
-        	index = Z_LVAL_P(zindex);
-        }
+		} else if (intern->type == TYPE_STRING_TO_MIXED) {
+			uint8_t     *key;
+			int         key_length;
+			zval        *value;
+			Pvoid_t     *PValue;
 
-        JLI(PValue, intern->array, index);
-        if (PValue != NULL && PValue != PJERR) {
-			if (*PValue != NULL) {
-				zval *old_value = (zval *)*PValue;
-				zval_ptr_dtor(&old_value);
+			if (zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC, "sz", &key, &key_length, &value) == FAILURE) {
+				RETURN_FALSE;
 			}
-            *PValue = value;
-            Z_ADDREF_P(*(zval **)PValue);
-            RETURN_TRUE;
-        } else {
-            RETURN_FALSE;
-        }
-    } else if (intern->type == TYPE_STRING_TO_INT) {
-        uint8_t     *key;
-        int         key_length;
-        Word_t      *value;
-        PWord_t     *PValue;
 
-        if (zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC, "sl", &key, &key_length, &value) == FAILURE) {
-            RETURN_FALSE;
-        }
-
-        JSLI(PValue, intern->array, key);
-        if (PValue != NULL && PValue != PJERR) {
-            *PValue = value;
-            intern->counter++;
-            RETURN_TRUE;
-        } else {
-            RETURN_FALSE;
-        }
-    } else if (intern->type == TYPE_STRING_TO_MIXED) {
-        uint8_t     *key;
-        int         key_length;
-        zval        *value;
-        Pvoid_t     *PValue;
-
-        if (zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC, "sz", &key, &key_length, &value) == FAILURE) {
-            RETURN_FALSE;
-        }
-
-        JSLI(PValue, intern->array, key);
-        if (PValue != NULL && PValue != PJERR) {
-			if (*PValue != NULL) {
-				zval *old_value = (zval *)*PValue;
-				zval_ptr_dtor(&old_value);
+			JSLI(PValue, intern->array, key);
+			if (PValue != NULL && PValue != PJERR) {
+				if (*PValue != NULL) {
+					zval *old_value = (zval *)*PValue;
+					zval_ptr_dtor(&old_value);
+				}
+				*PValue = value;
+				Z_ADDREF_P(value);
+				intern->counter++;
+				RETURN_TRUE;
+			} else {
+				RETURN_FALSE;
 			}
-            *PValue = value;
-            Z_ADDREF_P(value);
-            intern->counter++;
-            RETURN_TRUE;
-        } else {
-            RETURN_FALSE;
-        }
-    }
-    RETURN_FALSE;
+		}
+	RETURN_FALSE;
 }
 /* }}} */
 
 /* {{{ proto int Judy::offsetSet(mixed offset)
- set the value at the given offset in the Judy Array */
+   set the value at the given offset in the Judy Array */
 PHP_METHOD(judy, offsetUnset)
 {
-    int         Rc_int;
+	int         Rc_int;
 
-    JUDY_METHOD_GET_OBJECT
+	JUDY_METHOD_GET_OBJECT
 
-    if (intern->type == TYPE_BITSET) {
-        Word_t      index;
+		if (intern->type == TYPE_BITSET) {
+			Word_t      index;
 
-        if (zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC, "l", &index) == FAILURE) {
-            RETURN_FALSE;
-        }
+			if (zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC, "l", &index) == FAILURE) {
+				RETURN_FALSE;
+			}
 
-        J1U(Rc_int, intern->array, index);
-    } else if (intern->type == TYPE_INT_TO_INT || intern->type == TYPE_INT_TO_MIXED) {
-        Word_t      index;
+			J1U(Rc_int, intern->array, index);
+		} else if (intern->type == TYPE_INT_TO_INT || intern->type == TYPE_INT_TO_MIXED) {
+			Word_t      index;
 
-        if (zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC, "l", &index) == FAILURE) {
-            RETURN_FALSE;
-        }
+			if (zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC, "l", &index) == FAILURE) {
+				RETURN_FALSE;
+			}
 
-        if (intern->type == TYPE_INT_TO_INT) {
-            JLD(Rc_int, intern->array, index);
-        } else {
-            Pvoid_t     *PValue;
-            JLG(PValue, intern->array, index);
-            if (PValue != NULL && PValue != PJERR) {
-				zval *value = (zval *)*PValue;
-				zval_ptr_dtor(&value);
-                JLD(Rc_int, intern->array, index);
-            }
-        }
-        if (Rc_int == 1)
-            intern->counter--;
-    } else if (intern->type == TYPE_STRING_TO_INT || intern->type == TYPE_STRING_TO_MIXED) {
-        uint8_t     *key;
-        int         key_length;
+			if (intern->type == TYPE_INT_TO_INT) {
+				JLD(Rc_int, intern->array, index);
+			} else {
+				Pvoid_t     *PValue;
+				JLG(PValue, intern->array, index);
+				if (PValue != NULL && PValue != PJERR) {
+					zval *value = (zval *)*PValue;
+					zval_ptr_dtor(&value);
+					JLD(Rc_int, intern->array, index);
+				}
+			}
+			if (Rc_int == 1)
+				intern->counter--;
+		} else if (intern->type == TYPE_STRING_TO_INT || intern->type == TYPE_STRING_TO_MIXED) {
+			uint8_t     *key;
+			int         key_length;
 
-        if (zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC, "s", &key, &key_length) == FAILURE) {
-            RETURN_FALSE;
-        }
+			if (zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC, "s", &key, &key_length) == FAILURE) {
+				RETURN_FALSE;
+			}
 
-        if (intern->type == TYPE_STRING_TO_INT) {
-            JSLD(Rc_int, intern->array, key);
-        } else {
-            Pvoid_t     *PValue;
-            JSLG(PValue, intern->array, key);
-            if (PValue != NULL && PValue != PJERR) {
-				zval *value = (zval *)*PValue;
-				zval_ptr_dtor(&value);
-                JSLD(Rc_int, intern->array, key);
-            }
-        }
-        if (Rc_int == 1)
-            intern->counter--;
-    }
+			if (intern->type == TYPE_STRING_TO_INT) {
+				JSLD(Rc_int, intern->array, key);
+			} else {
+				Pvoid_t     *PValue;
+				JSLG(PValue, intern->array, key);
+				if (PValue != NULL && PValue != PJERR) {
+					zval *value = (zval *)*PValue;
+					zval_ptr_dtor(&value);
+					JSLD(Rc_int, intern->array, key);
+				}
+			}
+			if (Rc_int == 1)
+				intern->counter--;
+		}
 
-    RETURN_BOOL(Rc_int);
+	RETURN_BOOL(Rc_int);
 }
 /* }}} */
 
 /* {{{ proto mixed Judy::offsetGet(mixed offset)
- set the value at the given offset in the Judy Array */
+   set the value at the given offset in the Judy Array */
 PHP_METHOD(judy, offsetGet)
 {
-    JUDY_METHOD_GET_OBJECT
+	JUDY_METHOD_GET_OBJECT
 
-    if (intern->type == TYPE_BITSET) {
-        Word_t  index;
-        int     Rc_int;
+		if (intern->type == TYPE_BITSET) {
+			Word_t  index;
+			int     Rc_int;
 
-        if (zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC, "l", &index) == FAILURE) {
-            RETURN_FALSE;
-        }
+			if (zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC, "l", &index) == FAILURE) {
+				RETURN_FALSE;
+			}
 
-        J1T(Rc_int, intern->array, index);
-        RETURN_BOOL(Rc_int);
-    } else if (intern->type == TYPE_INT_TO_INT || intern->type == TYPE_INT_TO_MIXED) {
-        Word_t    index;
-        Word_t    *PValue;
+			J1T(Rc_int, intern->array, index);
+			RETURN_BOOL(Rc_int);
+		} else if (intern->type == TYPE_INT_TO_INT || intern->type == TYPE_INT_TO_MIXED) {
+			Word_t    index;
+			Word_t    *PValue;
 
-        if (zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC, "l", &index) == FAILURE) {
-            RETURN_FALSE;
-        }
+			if (zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC, "l", &index) == FAILURE) {
+				RETURN_FALSE;
+			}
 
-        JLG(PValue, intern->array, index);
-        if (PValue != NULL && PValue != PJERR) {
-            if (intern->type == TYPE_INT_TO_INT) {
-                RETURN_LONG(*PValue);
-            } else {
-                RETURN_ZVAL(*((zval **)PValue), 1, 0);
-            }
-        }
-    } else if (intern->type == TYPE_STRING_TO_INT || intern->type == TYPE_STRING_TO_MIXED) {
-        uint8_t     *key;
-        int         key_length;
-        Word_t      *PValue;
+			JLG(PValue, intern->array, index);
+			if (PValue != NULL && PValue != PJERR) {
+				if (intern->type == TYPE_INT_TO_INT) {
+					RETURN_LONG(*PValue);
+				} else {
+					RETURN_ZVAL(*((zval **)PValue), 1, 0);
+				}
+			}
+		} else if (intern->type == TYPE_STRING_TO_INT || intern->type == TYPE_STRING_TO_MIXED) {
+			uint8_t     *key;
+			int         key_length;
+			Word_t      *PValue;
 
-        if (zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC, "s", &key, &key_length) == FAILURE) {
-            RETURN_FALSE;
-        }
+			if (zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC, "s", &key, &key_length) == FAILURE) {
+				RETURN_FALSE;
+			}
 
-        JSLG(PValue, intern->array, key);
-        if (PValue != NULL && PValue != PJERR) {
-            if (intern->type == TYPE_STRING_TO_INT) {
-                RETURN_LONG(*PValue);
-            } else {
-                RETURN_ZVAL(*((zval **)PValue), 1 ,0);
-            }
-        }
-    }
+			JSLG(PValue, intern->array, key);
+			if (PValue != NULL && PValue != PJERR) {
+				if (intern->type == TYPE_STRING_TO_INT) {
+					RETURN_LONG(*PValue);
+				} else {
+					RETURN_ZVAL(*((zval **)PValue), 1 ,0);
+				}
+			}
+		}
 
-    RETURN_NULL();
+	RETURN_NULL();
 }
 /* }}} */
 
 /* {{{ proto int Judy::offsetExists(mixed offset)
- set the value at the given offset in the Judy Array */
+   set the value at the given offset in the Judy Array */
 PHP_METHOD(judy, offsetExists)
 {
-    JUDY_METHOD_GET_OBJECT
+	JUDY_METHOD_GET_OBJECT
 
-    if (intern->type == TYPE_BITSET) {
-        Word_t  index;
-        int     Rc_int;
+		if (intern->type == TYPE_BITSET) {
+			Word_t  index;
+			int     Rc_int;
 
-        if (zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC, "l", &index) == FAILURE) {
-            RETURN_FALSE;
-        }
+			if (zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC, "l", &index) == FAILURE) {
+				RETURN_FALSE;
+			}
 
-        J1T(Rc_int, intern->array, index);
-        RETURN_BOOL(Rc_int);
-    } else if (intern->type == TYPE_INT_TO_INT || intern->type == TYPE_INT_TO_MIXED) {
-        Word_t    index;
-        Word_t    *PValue;
+			J1T(Rc_int, intern->array, index);
+			RETURN_BOOL(Rc_int);
+		} else if (intern->type == TYPE_INT_TO_INT || intern->type == TYPE_INT_TO_MIXED) {
+			Word_t    index;
+			Word_t    *PValue;
 
-        if (zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC, "l", &index) == FAILURE) {
-            RETURN_FALSE;
-        }
+			if (zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC, "l", &index) == FAILURE) {
+				RETURN_FALSE;
+			}
 
-        JLG(PValue, intern->array, index);
-        if (PValue != NULL && PValue != PJERR) {
-            RETURN_TRUE;
-        }
-    } else if (intern->type == TYPE_STRING_TO_INT || intern->type == TYPE_STRING_TO_MIXED) {
-        uint8_t     *key;
-        int         key_length;
-        Word_t      *PValue;
+			JLG(PValue, intern->array, index);
+			if (PValue != NULL && PValue != PJERR) {
+				RETURN_TRUE;
+			}
+		} else if (intern->type == TYPE_STRING_TO_INT || intern->type == TYPE_STRING_TO_MIXED) {
+			uint8_t     *key;
+			int         key_length;
+			Word_t      *PValue;
 
-        if (zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC, "s", &key, &key_length) == FAILURE) {
-            RETURN_FALSE;
-        }
+			if (zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC, "s", &key, &key_length) == FAILURE) {
+				RETURN_FALSE;
+			}
 
-        JSLG(PValue, intern->array, key);
-        if (PValue != NULL && PValue != PJERR) {
-            RETURN_TRUE;
-        }
-    }
+			JSLG(PValue, intern->array, key);
+			if (PValue != NULL && PValue != PJERR) {
+				RETURN_TRUE;
+			}
+		}
 
-    RETURN_FALSE;
+	RETURN_FALSE;
 }
 /* }}} */
 
--- judy_handlers.c
+++ judy_handlers.c
@@ -20,105 +20,105 @@
 #include "judy_handlers.h"
 
 /* {{{ judy_object_count
- */
+*/
 int judy_object_count(zval *object, long *count TSRMLS_DC)
 {
-    zval *rv;
+	zval *rv;
 
-    /* calling the object's count() method */
-    zend_call_method_with_0_params(&object, NULL, NULL, "count", &rv);
-    *count = Z_LVAL_P(rv);
+	/* calling the object's count() method */
+	zend_call_method_with_0_params(&object, NULL, NULL, "count", &rv);
+	*count = Z_LVAL_P(rv);
 
-    /* destruct the zval returned value */
-    zval_ptr_dtor(&rv);
+	/* destruct the zval returned value */
+	zval_ptr_dtor(&rv);
 
-    return SUCCESS;
+	return SUCCESS;
 }
 /* }}} */
 
 /* {{{ judy_object_clone
- */
+*/
 zend_object_value judy_object_clone(zval *this_ptr TSRMLS_DC)
 {
-    judy_object *new_obj = NULL;
-    judy_object *old_obj = (judy_object *) zend_object_store_get_object(this_ptr TSRMLS_CC);
-    zend_object_value new_ov = judy_object_new_ex(old_obj->std.ce, &new_obj TSRMLS_CC);
-    
-    /* new Judy array to populate */
-    Pvoid_t newJArray = (Pvoid_t) NULL;
+	judy_object *new_obj = NULL;
+	judy_object *old_obj = (judy_object *) zend_object_store_get_object(this_ptr TSRMLS_CC);
+	zend_object_value new_ov = judy_object_new_ex(old_obj->std.ce, &new_obj TSRMLS_CC);
 
-    zend_objects_clone_members(&new_obj->std, new_ov, &old_obj->std, Z_OBJ_HANDLE_P(this_ptr) TSRMLS_CC);
+	/* new Judy array to populate */
+	Pvoid_t newJArray = (Pvoid_t) NULL;
 
-    if (old_obj->type == TYPE_BITSET) {
-        /* Cloning Judy1 Array */
+	zend_objects_clone_members(&new_obj->std, new_ov, &old_obj->std, Z_OBJ_HANDLE_P(this_ptr) TSRMLS_CC);
 
-        /* Key/index */
-        Word_t  kindex = 0;
+	if (old_obj->type == TYPE_BITSET) {
+		/* Cloning Judy1 Array */
 
-        /* Insert return value */
-        int     Rc_int = 0;
+		/* Key/index */
+		Word_t  kindex = 0;
 
-        J1F(Rc_int, old_obj->array, kindex);
-        while (Rc_int == 1)
-        {
-            J1S(Rc_int, newJArray, kindex);
-            J1N(Rc_int, newJArray, kindex);
-        }
-    } else if (old_obj->type == TYPE_INT_TO_INT || old_obj->type == TYPE_INT_TO_MIXED) {
-        /* Cloning JudyL Array */
+		/* Insert return value */
+		int     Rc_int = 0;
 
-        /* Key/index */
-        Word_t kindex = 0;
+		J1F(Rc_int, old_obj->array, kindex);
+		while (Rc_int == 1)
+		{
+			J1S(Rc_int, newJArray, kindex);
+			J1N(Rc_int, newJArray, kindex);
+		}
+	} else if (old_obj->type == TYPE_INT_TO_INT || old_obj->type == TYPE_INT_TO_MIXED) {
+		/* Cloning JudyL Array */
 
-        /* Pointer to the old value */
-        Word_t *PValue;
+		/* Key/index */
+		Word_t kindex = 0;
 
-        /* Pointer to the new value */
-        Word_t *newPValue;
+		/* Pointer to the old value */
+		Word_t *PValue;
 
-        JLF(PValue, old_obj->array, kindex);
-        while(PValue != NULL && PValue != PJERR)
-        {
-            JLI(newPValue, newJArray, kindex);
-            if (newPValue != NULL && newPValue != PJERR) {
-                *newPValue = *PValue;
-                if (old_obj->type == TYPE_INT_TO_MIXED)
-                    Z_ADDREF_P(*(zval **)PValue);
-            }
-            JLN(PValue, old_obj->array, kindex)
-        }
-    } else if (old_obj->type == TYPE_STRING_TO_INT || old_obj->type == TYPE_STRING_TO_MIXED) {
-        /* Cloning JudySL Array */
+		/* Pointer to the new value */
+		Word_t *newPValue;
 
-        /* Key/index */
-        uint8_t kindex[PHP_JUDY_MAX_LENGTH];
+		JLF(PValue, old_obj->array, kindex);
+		while(PValue != NULL && PValue != PJERR)
+		{
+			JLI(newPValue, newJArray, kindex);
+			if (newPValue != NULL && newPValue != PJERR) {
+				*newPValue = *PValue;
+				if (old_obj->type == TYPE_INT_TO_MIXED)
+					Z_ADDREF_P(*(zval **)PValue);
+			}
+			JLN(PValue, old_obj->array, kindex)
+		}
+	} else if (old_obj->type == TYPE_STRING_TO_INT || old_obj->type == TYPE_STRING_TO_MIXED) {
+		/* Cloning JudySL Array */
 
-        /* Pointer to the old value */
-        Word_t *PValue;
+		/* Key/index */
+		uint8_t kindex[PHP_JUDY_MAX_LENGTH];
 
-        /* Pointer to the new value */
-        Word_t *newPValue;
+		/* Pointer to the old value */
+		Word_t *PValue;
 
-        /* smallest string is a null-terminated character */
-        kindex[0] = '\0';
+		/* Pointer to the new value */
+		Word_t *newPValue;
 
-        JSLF(PValue, old_obj->array, kindex);
-        while(PValue != NULL && PValue != PJERR)
-        {
-            JSLI(newPValue, newJArray, kindex);
-            if (newPValue != NULL && newPValue != PJERR) {
-                *newPValue = *PValue;
-                if (old_obj->type == TYPE_STRING_TO_MIXED)
-                    Z_ADDREF_P(*(zval **)PValue);
-            }
-            JSLN(PValue, old_obj->array, kindex)
-        }
-    }
+		/* smallest string is a null-terminated character */
+		kindex[0] = '\0';
 
-    new_obj->array = newJArray;
-    new_obj->type = old_obj->type;
+		JSLF(PValue, old_obj->array, kindex);
+		while(PValue != NULL && PValue != PJERR)
+		{
+			JSLI(newPValue, newJArray, kindex);
+			if (newPValue != NULL && newPValue != PJERR) {
+				*newPValue = *PValue;
+				if (old_obj->type == TYPE_STRING_TO_MIXED)
+					Z_ADDREF_P(*(zval **)PValue);
+			}
+			JSLN(PValue, old_obj->array, kindex)
+		}
+	}
 
-    return new_ov;
+	new_obj->array = newJArray;
+	new_obj->type = old_obj->type;
+
+	return new_ov;
 }
 /* }}} */
 
